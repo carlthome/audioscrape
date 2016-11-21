4f0ab44e9cad7f9812a22357d4db7277c498949e
Carl Thomé <carlthome@gmail.com>
2016-11-21 17:37:24 +0100

    Add classifiers

--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
 # coding=utf-8
-"""Install audio scraper."""
+"""Install config."""
 from setuptools import find_packages, setup
 
 setup(
     name='audioscrape',
     version='0.0.0',
-    description='Scrape audio from various websites.',
-    license='MIT',
+    description='Scrape audio from various websites with a simple command-line interface.',
+    url='https://github.com/carlthome/audio-scraper',
     author='Carl Thomé',
     author_email='carlthome@gmail.com',
-    url='https://github.com/carlthome/audio-scraper',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Multimedia :: Sound/Audio',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='audio music sound',
     packages=find_packages(),
     install_requires=[
         'youtube-dl', 'pafy', 'tqdm', 'requests', 'soundcloud', 'six'
