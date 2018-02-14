e26dcfa1b3f3aa0e89f37c42bd89758637d4dc1e
Carl Thomé <carlthome@gmail.com>
2018-02-14 14:40:16 +0100

    Remove pytest specifics

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='audioscrape',
-    version='0.0.4',
+    version='0.0.5',
     description='Scrape audio from various websites with a simple command-line interface.',
     url='https://github.com/carlthome/audio-scraper',
     author='Carl Thomé',
@@ -19,15 +19,18 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     keywords='audio music sound',
     packages=find_packages(),
     install_requires=[
-        'youtube-dl', 'pafy', 'tqdm', 'requests', 'soundcloud', 'six'
+        'youtube-dl',
+        'pafy',
+        'tqdm',
+        'requests',
+        'soundcloud',
+        'six',
     ],
     entry_points={
         'console_scripts': ['audioscrape = audioscrape.__main__:main']
-    },
-    include_package_data=True,
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'])
+    })
