aec9095465a9827f010444f9e2c9d0e91fcc6cf0
Carl Thomé <carlthome@gmail.com>
2023-06-25 12:14:32 +0200

    Add verbose flag

--- audioscrape/__main__.py
+++ audioscrape/__main__.py
@@ -1,14 +1,17 @@
 """Download audio."""
 import argparse
+import logging
 import sys
 
 from . import soundcloud, youtube
 
+logger = logging.getLogger(__name__)
 
-def download(query, include=None, exclude=None, quiet=False, overwrite=False):
+
+def download(query, include=None, exclude=None, quiet=False, verbose=False, overwrite=False):
     """Scrape various websites for audio."""
-    youtube.scrape(query, include, exclude, quiet, overwrite)
-    soundcloud.scrape(query, include, exclude, quiet, overwrite)
+    youtube.scrape(query, include, exclude, quiet, verbose, overwrite)
+    soundcloud.scrape(query, include, exclude, quiet, verbose, overwrite)
 
 
 def cli(args=None):
@@ -19,14 +22,21 @@ def cli(args=None):
     parser.add_argument("-i", "--include", default=[], action="append", help="only download audio with this tag")
     parser.add_argument("-e", "--exclude", default=[], action="append", help="ignore results with this tag")
     parser.add_argument("-q", "--quiet", default=False, action="store_true", help="hide progress reporting")
+    parser.add_argument("-v", "--verbose", default=False, action="store_true", help="display debug information")
     parser.add_argument("-o", "--overwrite", default=False, action="store_true", help="overwrite existing files")
     args = parser.parse_args()
 
-    if not args.quiet:
-        print('Downloading audio from "{}" videos tagged {} and not {}.'.format(args.query, args.include, args.exclude))
-    download(args.query, args.include, args.exclude, args.quiet, args.overwrite)
-    if not args.quiet:
-        print("Finished downloading audio.")
+    logging.basicConfig(format="[%(name)s] %(message)s")
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+    elif args.quiet:
+        logger.setLevel(logging.ERROR)
+    else:
+        logger.setLevel(logging.INFO)
+
+    logger.info(f'Downloading audio from "{args.query}" videos tagged {args.include} and not {args.exclude}.')
+    download(args.query, args.include, args.exclude, args.quiet, args.verbose, args.overwrite)
+    logger.info("Finished downloading audio.")
 
 
 if __name__ == "__main__":
